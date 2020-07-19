78f627cff46662b9f2e90a858cb77764174b93ac
dudziakpudziak <lukaszdudziak777@gmail.com>
2020-07-19 17:43:59 +0200

    final_javaScript

--- Projekty/Projet_13_OOP/Draw.js
+++ Projekty/Projet_13_OOP/Draw.js
@@ -12,6 +12,7 @@ class Draw {
             const index = Math.floor(Math.random() * this.options.length)
             const color = this.options[index];
             colors.push(color)
+            // console.log(color)
         }
         return colors
     }
